a8531f214acce1928d441c655cf5adcefd1e93e5
Laszlo Martonfy <martonfylaszlo@gmail.com>
2020-02-07 14:39:51 +0100

    Dependency injection containerization
    Configures Caliburn Micro for MVVM framework
     and adds Simple Container DI system.

--- TRMWPFUserInterface/Bootstrapper.cs
+++ TRMWPFUserInterface/Bootstrapper.cs
@@ -11,11 +11,27 @@ namespace TRMDesktopUI
 {
     public class Bootstrapper : BootstrapperBase
     {
-        //private SimpleContainer _container = new SimpleContainer();
+        private SimpleContainer _container = new SimpleContainer();
 
         public Bootstrapper()
         {
             Initialize();
+        }
+
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _container.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));
         }
 
         protected override void OnStartup(object sender, StartupEventArgs e)
@@ -23,19 +39,19 @@ namespace TRMDesktopUI
             DisplayRootViewFor<ShellViewModel>();
         }
 
-        //protected override object GetInstance(Type service, string key)
-        //{
-        //    return _container.GetInstance(service, key);  
-        //}
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
 
-        //protected override IEnumerable<object> GetAllInstances(Type service)
-        //{
-        //    return _container.GetAllInstances(service);
-        //}
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
 
-        //protected override void BuildUp(object instance)
-        //{
-        //    _container.BuildUp(instance);
-        //}
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
     }
 }
--- TRMWPFUserInterface/ViewModels/ShellViewModel.cs
+++ TRMWPFUserInterface/ViewModels/ShellViewModel.cs
@@ -8,5 +8,6 @@ namespace TRMDesktopUI.ViewModels
 {
     public class ShellViewModel
     {
+
     }
 }
