eb10bce4dd346c0f8b18b656ba8b648a8f26e396
Laszlo Martonfy <martonfylaszlo@gmail.com>
2020-02-08 14:31:45 +0100

    Register DB Tables
    The tables for the basic register system

---
+++ TRMData/dbo/Tables/Inventory.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Inventory]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[ProductId] INT NOT NULL,
+	[Quantity] INT NOT NULL DEFAULT 1,
+	[PurchasePrice] MONEY NOT NULL,
+	[PurchaseDate] DATETIME2(7) NOT NULL DEFAULT getutcDATE()
+
+)
