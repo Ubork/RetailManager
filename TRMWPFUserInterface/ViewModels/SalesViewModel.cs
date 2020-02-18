948e484cb6f8c50a9cab5d2fd857fe98d1e89816
Laszlo Martonfy <martonfylaszlo@gmail.com>
2020-02-18 15:30:48 +0100

    Updated Remove Item functionality
    Fixed the remove item button functionality to update
    the Cart and Item qty properly.

--- TRMWPFUserInterface/ViewModels/SalesViewModel.cs
+++ TRMWPFUserInterface/ViewModels/SalesViewModel.cs
@@ -15,257 +15,253 @@ namespace TRMDesktopUI.ViewModels
 {
     public class SalesViewModel : Screen
     {
-		private BindingList<ProductDisplayModel> _products;
-		private BindingList<CartItemDisplayModel> _cart = new BindingList<CartItemDisplayModel>();
-		private int _itemQuantity = 1;
-		private ProductDisplayModel _selectedProduct;
-		private CartItemDisplayModel _selectedItemToRemove;
+        private BindingList<ProductDisplayModel> _products;
+        private BindingList<CartItemDisplayModel> _cart = new BindingList<CartItemDisplayModel>();
+        private int _itemQuantity = 1;
+        private ProductDisplayModel _selectedProduct;
+        private CartItemDisplayModel _selectedItemToRemove;
 
-		IProductEndpoint _productEndpoint;
-		IConfigHelper _configHelper;
-		ISaleEndpoint _saleEndpoint;
-		IMapper _mapper;
-		public SalesViewModel(IProductEndpoint productEndpoint, IConfigHelper configHelper,
-			ISaleEndpoint saleEndpoint, IMapper mapper)
-		{
-			_productEndpoint = productEndpoint;
-			_configHelper = configHelper;
-			_saleEndpoint = saleEndpoint;
-			_mapper = mapper;
-		}
+        IProductEndpoint _productEndpoint;
+        IConfigHelper _configHelper;
+        ISaleEndpoint _saleEndpoint;
+        IMapper _mapper;
+        public SalesViewModel(IProductEndpoint productEndpoint, IConfigHelper configHelper,
+            ISaleEndpoint saleEndpoint, IMapper mapper)
+        {
+            _productEndpoint = productEndpoint;
+            _configHelper = configHelper;
+            _saleEndpoint = saleEndpoint;
+            _mapper = mapper;
+        }
 
-		protected override async void OnViewLoaded(object view)
-		{
-			await LoadProducts();
-			base.OnViewLoaded(view);
-		}
+        protected override async void OnViewLoaded(object view)
+        {
+            await LoadProducts();
+            base.OnViewLoaded(view);
+        }
 
-		private async Task LoadProducts()
-		{
-			var productList = await _productEndpoint.GetAll();
-			var products = _mapper.Map<List<ProductDisplayModel>>(productList);
-			Products = new BindingList<ProductDisplayModel>(products);
-		}
+        private async Task LoadProducts()
+        {
+            var productList = await _productEndpoint.GetAll();
+            var products = _mapper.Map<List<ProductDisplayModel>>(productList);
+            Products = new BindingList<ProductDisplayModel>(products);
+        }
 
-		public BindingList<ProductDisplayModel> Products
-		{
-			get { return _products; }
-			set 
-			{ 
-				_products = value;
-				NotifyOfPropertyChange(() => Products);
-			}
-		}
+        public BindingList<ProductDisplayModel> Products
+        {
+            get { return _products; }
+            set
+            {
+                _products = value;
+                NotifyOfPropertyChange(() => Products);
+            }
+        }
 
-		public ProductDisplayModel SelectedProduct
-		{
-			get { return _selectedProduct; }
-			set
-			{
-				_selectedProduct = value;
-				NotifyOfPropertyChange(() => SelectedProduct);
-				NotifyOfPropertyChange(() => CanAddToCart);
-			}
-		}
+        public ProductDisplayModel SelectedProduct
+        {
+            get { return _selectedProduct; }
+            set
+            {
+                _selectedProduct = value;
+                NotifyOfPropertyChange(() => SelectedProduct);
+                NotifyOfPropertyChange(() => CanAddToCart);
+            }
+        }
 
-		public CartItemDisplayModel SelectedItemToRemove
-		{
-			get { return _selectedItemToRemove; }
-			set 
-			{
-				_selectedItemToRemove = value;
-				NotifyOfPropertyChange(() => SelectedItemToRemove);
-				NotifyOfPropertyChange(() => CanRemoveFromCart);
-			}
-		}
+        public CartItemDisplayModel SelectedItemToRemove
+        {
+            get { return _selectedItemToRemove; }
+            set
+            {
+                _selectedItemToRemove = value;
+                NotifyOfPropertyChange(() => SelectedItemToRemove);
+                NotifyOfPropertyChange(() => CanRemoveFromCart);
+            }
+        }
 
-		public int ItemQuantity
-		{
-			get { return _itemQuantity; }
-			set 
-			{
-				_itemQuantity = value;
-				NotifyOfPropertyChange(() => ItemQuantity);
-				NotifyOfPropertyChange(() => CanAddToCart);
-			}
-		}
-
-
-		public BindingList<CartItemDisplayModel> Cart
-		{
-			get { return _cart; }
-			set 
-			{
-				_cart = value;
-				NotifyOfPropertyChange(() => Cart);
-			}
-		}
-
-		public string SubTotal 
-		{
-			get
-			{
-				return CalculateSubTotal().ToString("C");
-			}
-		}
-
-		private decimal CalculateSubTotal()
-		{
-			decimal subTotal = 0;
-			foreach (var item in Cart)
-			{
-				subTotal += (item.Product.RetailPrice * item.QuantityInCart);
-			}
-			return subTotal;
-		}
-
-		public string Tax
-		{
-			get
-			{
-				return CalculateTax().ToString("C");
-			}
-		}
-
-		private decimal CalculateTax()
-		{
-			decimal taxAmount = 0;
-			decimal taxRate = _configHelper.GetTaxRate();
-
-			taxAmount = Cart.Where(x => x.Product.IsTaxable)
-				.Sum(x => x.Product.RetailPrice * x.QuantityInCart * taxRate * (decimal)0.01);
-
-			//foreach (var item in Cart)
-			//{
-			//	if (item.Product.IsTaxable)
-			//	{
-			//		taxAmount += (item.Product.RetailPrice * item.QuantityInCart * taxRate * (decimal)0.01);
-			//	}
-			//}
-			
-			return taxAmount;
-		}
-
-		public string Total
-		{
-			get
-			{
-				decimal total = CalculateSubTotal() + CalculateTax();
-				return total.ToString("C");
-			}
-		}
-
-		public void AddToCart()
-		{
-			CartItemDisplayModel existingItem = Cart.FirstOrDefault(x => x.Product == SelectedProduct);
-
-			if (existingItem != null)
-			{
-				existingItem.QuantityInCart += ItemQuantity;
-			}
-			else
-			{
-				CartItemDisplayModel item = new CartItemDisplayModel
-				{
-					Product = SelectedProduct,
-					QuantityInCart = ItemQuantity
-				};
-				Cart.Add(item);
-			}
-			SelectedProduct.QuantityInStock -= ItemQuantity;
-			ItemQuantity = 1;
-			NotifyOfPropertyChange(() => SubTotal);
-			NotifyOfPropertyChange(() => Tax);
-			NotifyOfPropertyChange(() => Total);
-			NotifyOfPropertyChange(() => CanCheckOut);
-
-		}
-
-		public bool CanAddToCart
-		{
-			get
-			{
-				bool output = false;
-
-				//Make sure something is in the cart
-				//Make sure the required item quantity is available
-				if (ItemQuantity > 0 && SelectedProduct?.QuantityInStock >= ItemQuantity)
-				{
-					output = true;
-				}
-				return output;
-			}
-		}
-
-		public void RemoveFromCart()
-		{
-			CartItemDisplayModel remainerOfItemToRemove = new CartItemDisplayModel
-			{
-				Product = SelectedItemToRemove.Product,
-				//QuantityInCart = SelectedItemToRemove.QuantityInCart - ItemQuantity
-			};
-
-			//if (ItemQuantity < SelectedItemToRemove.QuantityInCart)
-			//{
-			//	Cart.Add(remainerOfItemToRemove);
-			//}
-
-			ProductDisplayModel restockItem = Products.FirstOrDefault(x => x.Equals(SelectedItemToRemove.Product));
-			Cart.Remove(SelectedItemToRemove);
-			SelectedProduct.QuantityInStock += ItemQuantity;
-			ItemQuantity = 1;
-			NotifyOfPropertyChange(() => SubTotal);
-			NotifyOfPropertyChange(() => Tax);
-			NotifyOfPropertyChange(() => Total);
-			NotifyOfPropertyChange(() => CanCheckOut);
-		}
-
-		public bool CanRemoveFromCart
-		{
-			get
-			{
-				bool output = false;
-
-				//Make sure something is in the cart
-				//Make sure the required item quantity is available
-				if (SelectedItemToRemove?.QuantityInCart >= ItemQuantity)
-				{
-					output = true;
-				}
-				return output;
-			}
-		}
-		public bool CanCheckOut
-		{
-			get
-			{
-				bool output = false;
-
-				//Make sure checkout is possible
-				if (Cart.Count > 0)
-				{
-					output = true;
-				}
-				return output;
-			}
-		}
-
-		public async void CheckOut()
-		{
-			SaleModel sale = new SaleModel();
-
-			foreach (var item in Cart)
-			{
-				sale.SaleDetails.Add(new SaleDetailModel
-				{
-					ProductId = item.Product.Id,
-					Quantity = item.QuantityInCart
-				});
-			}
-
-			await _saleEndpoint.PostSale(sale);
-		}
+        public int ItemQuantity
+        {
+            get { return _itemQuantity; }
+            set
+            {
+                _itemQuantity = value;
+                NotifyOfPropertyChange(() => ItemQuantity);
+                NotifyOfPropertyChange(() => CanAddToCart);
+            }
+        }
 
 
-	}
+        public BindingList<CartItemDisplayModel> Cart
+        {
+            get { return _cart; }
+            set
+            {
+                _cart = value;
+                NotifyOfPropertyChange(() => Cart);
+            }
+        }
+
+        public string SubTotal
+        {
+            get
+            {
+                return CalculateSubTotal().ToString("C");
+            }
+        }
+
+        private decimal CalculateSubTotal()
+        {
+            decimal subTotal = 0;
+            foreach (var item in Cart)
+            {
+                subTotal += (item.Product.RetailPrice * item.QuantityInCart);
+            }
+            return subTotal;
+        }
+
+        public string Tax
+        {
+            get
+            {
+                return CalculateTax().ToString("C");
+            }
+        }
+
+        private decimal CalculateTax()
+        {
+            decimal taxAmount = 0;
+            decimal taxRate = _configHelper.GetTaxRate();
+
+            taxAmount = Cart.Where(x => x.Product.IsTaxable)
+                .Sum(x => x.Product.RetailPrice * x.QuantityInCart * taxRate * (decimal)0.01);
+
+            //foreach (var item in Cart)
+            //{
+            //	if (item.Product.IsTaxable)
+            //	{
+            //		taxAmount += (item.Product.RetailPrice * item.QuantityInCart * taxRate * (decimal)0.01);
+            //	}
+            //}
+
+            return taxAmount;
+        }
+
+        public string Total
+        {
+            get
+            {
+                decimal total = CalculateSubTotal() + CalculateTax();
+                return total.ToString("C");
+            }
+        }
+
+        public void AddToCart()
+        {
+            CartItemDisplayModel existingItem = Cart.FirstOrDefault(x => x.Product == SelectedProduct);
+
+            if (existingItem != null)
+            {
+                existingItem.QuantityInCart += ItemQuantity;
+            }
+            else
+            {
+                CartItemDisplayModel item = new CartItemDisplayModel
+                {
+                    Product = SelectedProduct,
+                    QuantityInCart = ItemQuantity
+                };
+                Cart.Add(item);
+            }
+            SelectedProduct.QuantityInStock -= ItemQuantity;
+            ItemQuantity = 1;
+            NotifyOfPropertyChange(() => SubTotal);
+            NotifyOfPropertyChange(() => Tax);
+            NotifyOfPropertyChange(() => Total);
+            NotifyOfPropertyChange(() => CanCheckOut);
+
+        }
+
+        public bool CanAddToCart
+        {
+            get
+            {
+                bool output = false;
+
+                //Make sure something is in the cart
+                //Make sure the required item quantity is available
+                if (ItemQuantity > 0 && SelectedProduct?.QuantityInStock >= ItemQuantity)
+                {
+                    output = true;
+                }
+                return output;
+            }
+        }
+
+        public void RemoveFromCart()
+        {
+            SelectedItemToRemove.QuantityInCart -= ItemQuantity;
+
+            ProductDisplayModel restockItem = Products.FirstOrDefault(x => x.Equals(SelectedItemToRemove.Product));
+            restockItem.QuantityInStock += ItemQuantity;
+            
+            if (SelectedItemToRemove.QuantityInCart == 0)
+            {
+                Cart.Remove(SelectedItemToRemove);
+            }
+            
+            ItemQuantity = 1;
+            NotifyOfPropertyChange(() => SubTotal);
+            NotifyOfPropertyChange(() => Tax);
+            NotifyOfPropertyChange(() => Total);
+            NotifyOfPropertyChange(() => CanCheckOut);
+        }
+
+        public bool CanRemoveFromCart
+        {
+            get
+            {
+                bool output = false;
+
+                //Make sure something is in the cart
+                //Make sure the required item quantity is available
+                if (SelectedItemToRemove?.QuantityInCart >= ItemQuantity)
+                {
+                    output = true;
+                }
+                return output;
+            }
+        }
+        public bool CanCheckOut
+        {
+            get
+            {
+                bool output = false;
+
+                //Make sure checkout is possible
+                if (Cart.Count > 0)
+                {
+                    output = true;
+                }
+                return output;
+            }
+        }
+
+        public async void CheckOut()
+        {
+            SaleModel sale = new SaleModel();
+
+            foreach (var item in Cart)
+            {
+                sale.SaleDetails.Add(new SaleDetailModel
+                {
+                    ProductId = item.Product.Id,
+                    Quantity = item.QuantityInCart
+                });
+            }
+
+            await _saleEndpoint.PostSale(sale);
+        }
+
+
+    }
 }
